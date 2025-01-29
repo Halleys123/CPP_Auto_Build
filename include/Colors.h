85c4e3088acfaf92abaeeae6fac42ce647408704
Arnav Chhabra <65032719+Halleys123@users.noreply.github.com>
2025-01-29 13:46:43 +0530

    Refactor color definitions; replace extern declarations with #define macros and remove Colors.cpp file from the build process

--- include/Colors.h
+++ include/Colors.h
@@ -1,17 +1,17 @@
 #pragma once
 
 // FOREGROUND
-extern const char *YELLOW_F;
-extern const char *RED_F;
-extern const char *WHITE_F;
-extern const char *GREY_F;
-extern const char *ORANGE_F;
-extern const char *DARK_GREY_F;
-extern const char *GREEN_F;
-extern const char *BLUE_F;
-extern const char *MEDIUM_STATE_BLUE_F;
-extern const char *RESET_F;
+#define YELLOW_F "\x1b[38;2;250;218;94m"
+#define RED_F "\x1b[38;2;255;56;0m"
+#define WHITE_F "\x1b[38;2;255;255;255m"
+#define GREY_F "\x1b[38;2;192;192;192m"
+#define ORANGE_F "\x1b[38;2;255;140;0m"
+#define DARK_GREY_F "\x1b[38;2;155;155;155m"
+#define GREEN_F "\x1b[38;2;63;255;0m"
+#define BLUE_F "\x1b[38;2;0;185;232m"
+#define MEDIUM_STATE_BLUE_F "\x1b[38;2;123;104;238m"
+#define RESET_F "\x1b[39m"
 
 // BACKGROUND
-extern const char *RESET_BACKGROUND;
-extern const char *DATE_BACKGROUND;
+#define RESET_BACKGROUND "\x1b[49m"
+#define DATE_BACKGROUND "\x1b[48;2;15;7;0m"
